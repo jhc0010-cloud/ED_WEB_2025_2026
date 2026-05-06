d633ec833a5386d48837b016bae2cb5c5e613ead
DavidMM <120629671+Dmm123d@users.noreply.github.com>
2026-05-06 12:14:02 +0200

    fix: revertir cambios en ClinicaVeterinariaApplication.java
    Removed password encoding from main method for security.

--- clinica-veterinaria/src/main/java/com/clinica/veterinaria/ClinicaVeterinariaApplication.java
+++ clinica-veterinaria/src/main/java/com/clinica/veterinaria/ClinicaVeterinariaApplication.java
@@ -2,14 +2,12 @@ package com.clinica.veterinaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ClinicaVeterinariaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClinicaVeterinariaApplication.class, args);
-        System.out.println(new BCryptPasswordEncoder().encode("1234"));
     }
 }
 
