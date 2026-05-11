8edec5e60c005fbd38fdf97786361071134d5acb
Gabii04 <gcruzjimenez43@gmail.com>
2026-05-11 16:29:13 +0200

    subida g2 trabajo

--- clinica-veterinaria/src/main/java/com/clinica/veterinaria/repository/RolRepository.java
+++ clinica-veterinaria/src/main/java/com/clinica/veterinaria/repository/RolRepository.java
@@ -1,10 +1,11 @@
 package com.clinica.veterinaria.repository;
 
+// Responsable backend: Rafael Santiago de la Torre Jimenez - G2, persistencia y consultas.
 import com.clinica.veterinaria.entity.Rol;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RolRepository extends JpaRepository<Rol, Long> {
-    Optional<Rol> findByNombre(String nombre);
+public interface RolRepository
+extends JpaRepository<Rol, Long> {
+    public Optional<Rol> findByNombre(String var1);
 }
-
