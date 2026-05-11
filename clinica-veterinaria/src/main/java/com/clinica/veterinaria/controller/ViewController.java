5ab45e16328874327339b789aed39656c2f8d473
Francisco7887 <franarmilla@gmail.com>
2026-05-11 20:24:56 +0200

    Errores html HTml +controllers

--- clinica-veterinaria/src/main/java/com/clinica/veterinaria/controller/ViewController.java
+++ clinica-veterinaria/src/main/java/com/clinica/veterinaria/controller/ViewController.java
@@ -1,14 +1,19 @@
 package com.clinica.veterinaria.controller;
 
+// Responsable backend: Juan Hakram Huertas Chergui - G1, capa de control y endpoints REST.
 import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-
-    @GetMapping("/")
+    @GetMapping(value={"/"})
     public String home() {
         return "redirect:/index.html";
     }
-}
 
+    @GetMapping(value={"/favicon.ico"})
+    public ResponseEntity<Void> favicon() {
+        return ResponseEntity.noContent().build();
+    }
+}
